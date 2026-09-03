dc9126c0faa504494a9d41999df08ab98c2861c9
Kerby Bautista <kerbybautista05@gmail.com>
2026-09-03 14:44:28 +0800

    Register PreventBackHistory middleware in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            PreventBackHistory::class,
         ]);
 
         $middleware->alias([
